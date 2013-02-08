d6b07b3e1680bfb5cfb7d1c16dc83813b9a04a6b
Robert Collins <robert.collins24@mail.dcu.ie>
2013-02-08 10:56:47 +0000

    Refactored package names.

--- src/main/java/ie/dcu/collinsrj/App.java
+++ src/main/java/ie/dcu/collinsrj/App.java
@@ -1,4 +1,4 @@
-package com.collinsrj;
+package ie.dcu.collinsrj;
 
 /**
  * Hello world!
--- src/test/java/ie/dcu/collinsrj/AppTest.java
+++ src/test/java/ie/dcu/collinsrj/AppTest.java
@@ -1,4 +1,4 @@
-package com.collinsrj;
+package ie.dcu.collinsrj;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
